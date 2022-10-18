fbf4c2a74aefea08e1ffbc9a9b691e18f0390c02
Edis <47144455+ESat777@users.noreply.github.com>
2022-10-18 23:33:29 +0300

    add router, page 404

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-index 
+<?php
+require_once "bootstrap.php";
+
+$request = $_SERVER['REQUEST_URI'];
+
+$appPathArray = explode('\\', getcwd());
+$appName = strtolower(end($appPathArray));
+
+
+switch ($request) {
+    case '/' . $appName . '/':
+      require __DIR__ . '/src/views/home.php';
+      break;
+    case '/' . $appName . '':
+      require __DIR__ . '/src/views/home.php';
+      break;
+    case '/' . $appName . '/home':
+      require __DIR__ . '/src/views/home.php';
+      break;
+    case '/' . $appName . '/admin':
+      require __DIR__ . '/src/views/admin.php';
+      break;
+    default:
+      http_response_code(404);
+      require __DIR__ . '/src/views/404.php';
+      break;
+    }
--- src/views/404.php
+++ src/views/404.php
@@ -1,1 +1,30 @@
-404 page
+<?php
+$appPathArray = explode('\\', getcwd());
+$appName = strtolower(end($appPathArray));
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous">    
+  <link rel="stylesheet" href="css/main.css">
+  <title>404 error</title>
+</head>
+
+<body>
+  <div class="container p-4 d-flex justify-content-center bg-light" style="height: 100vh;">
+    <div style="width: 800px;	text-align: center;">
+        <h1 style="font-size: 160px; color: #f8c471;">404</h1>
+        <div>Looks like this page is missing. Don't worry tough, our best main is on the case.</div>
+        <div>Meanwhile, why don't try again by going</div><br>
+        <?php
+        echo '<a class="btn btn-primary" href="/' . $appName . '">Back home</a>';
+        ?>
+    </div>
+  </div>
+</body>
